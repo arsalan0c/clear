fbbcdeaea8fe6bc17c8454bb1e9b99b29b5ae8ef
Arsalan Cheema <arsalanc.v2@gmail.com>
2019-06-01 09:42:24 +0800

    move functions to separate file

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+// background.js
+
+import {createTab, executeFunction, sleep, closeTab, focusTab, doesTabExist, clickDeleteAllButton, clickConfirmDeleteButton, formScript} from './helper.js';
+
 const SEARCH_ACTIVITY_URL = "https://myactivity.google.com/privacyadvisor/search";
 const SLEEP_DURATION = 1000;
 
@@ -29,70 +33,3 @@ chrome.browserAction.onClicked.addListener((originalTab) => {
 		.then(() => alert(POS_RESULT_MSG))
 		.catch(err => alert(err));
 });
-
-let createTab = (url, err) => {
-	return new Promise((resolve, reject) => {
-		return chrome.tabs.create({url: url, selected: true}, resolve);
-	}).catch(() => Promise.reject(err));
-};
-
-let executeFunction = (tabId, script, err) => {
-	return new Promise((resolve, reject) => {
-		chrome.tabs.executeScript(tabId, {code: script}, resolve);
-	}).catch(() => Promise.reject(err));
-};
-
-let sleep = ms => new Promise((resolve, reject) => {
-	setTimeout(resolve, ms);
-});
-
-let closeTab = (tabId, err) => {
-	return new Promise((resolve, reject) => {
-		chrome.tabs.remove(tabId, resolve);
-	}).catch(() => Promise.reject(err));
-};
-
-let focusTab = (tabId, err) => {
-	return new Promise((resolve, reject) => {
-		chrome.tabs.update(tabId, {selected: true}, resolve);
-	}).catch(() => Promise.reject(err));
-};
-
-let doesTabExist = (tabId) => {
-	return new Promise((resolve, reject) => {
-		chrome.tabs.get(tabId, resolve);
-	}).catch(() => false);
-};
-
-let clickDeleteAllButton = () => {
-	// EXTENSION WON'T WORK IF THESE CHANGE
-	var DELETE_ALL_SELECTOR = "jsname";
-	var DELETE_ALL_SELECTOR_VALUE = "dQulXd";
-
-	let button = document.querySelector("[" + DELETE_ALL_SELECTOR + "=" + CSS.escape(DELETE_ALL_SELECTOR_VALUE) + "]");
-	if (button) button.click();
-};
-
-let clickConfirmDeleteButton = () => {
-	// EXTENSION WON'T WORK IF THESE CHANGE
-	var DELETE_CONFIRM_SELECTOR = "data-id";
-	var DELETE_CONFIRM_SELECTOR_VALUE = "EBS5u";
-
-	let button = document.querySelector("[" + DELETE_CONFIRM_SELECTOR + "=" + CSS.escape(DELETE_CONFIRM_SELECTOR_VALUE) + "]");
-	if (button) button.click();
-};
-
-let formScript = (func, args) => {
-	let script = "(" + func + ")";
-	if (args && args.length > 0) {
-		args.forEach(element => {
-			script += "(" + element + ")";
-		});
-	} else {
-		// call the function
-		script += "()";
-	}
-
-	script = `${script}`;
-	return script;
-};
---
+++ helper.js
@@ -0,0 +1,70 @@
+// helper.js
+
+let createTab = (url, err) => {
+	return new Promise((resolve, reject) => {
+		return chrome.tabs.create({url: url, selected: true}, resolve);
+	}).catch(() => Promise.reject(err));
+};
+
+let executeFunction = (tabId, script, err) => {
+	return new Promise((resolve, reject) => {
+		chrome.tabs.executeScript(tabId, {code: script}, resolve);
+	}).catch(() => Promise.reject(err));
+};
+
+let sleep = ms => new Promise((resolve, reject) => {
+	setTimeout(resolve, ms);
+});
+
+let closeTab = (tabId, err) => {
+	return new Promise((resolve, reject) => {
+		chrome.tabs.remove(tabId, resolve);
+	}).catch(() => Promise.reject(err));
+};
+
+let focusTab = (tabId, err) => {
+	return new Promise((resolve, reject) => {
+		chrome.tabs.update(tabId, {selected: true}, resolve);
+	}).catch(() => Promise.reject(err));
+};
+
+let doesTabExist = (tabId) => {
+	return new Promise((resolve, reject) => {
+		chrome.tabs.get(tabId, resolve);
+	}).catch(() => false);
+};
+
+let clickDeleteAllButton = () => {
+	// EXTENSION WON'T WORK IF THESE CHANGE
+	var DELETE_ALL_SELECTOR = "jsname";
+	var DELETE_ALL_SELECTOR_VALUE = "dQulXd";
+
+	let button = document.querySelector("[" + DELETE_ALL_SELECTOR + "=" + CSS.escape(DELETE_ALL_SELECTOR_VALUE) + "]");
+	if (button) button.click();
+};
+
+let clickConfirmDeleteButton = () => {
+	// EXTENSION WON'T WORK IF THESE CHANGE
+	var DELETE_CONFIRM_SELECTOR = "data-id";
+	var DELETE_CONFIRM_SELECTOR_VALUE = "EBS5u";
+
+	let button = document.querySelector("[" + DELETE_CONFIRM_SELECTOR + "=" + CSS.escape(DELETE_CONFIRM_SELECTOR_VALUE) + "]");
+	if (button) button.click();
+};
+
+let formScript = (func, args) => {
+	let script = "(" + func + ")";
+	if (args && args.length > 0) {
+		args.forEach(element => {
+			script += "(" + element + ")";
+		});
+	} else {
+		// call the function
+		script += "()";
+	}
+
+	script = `${script}`;
+	return script;
+};
+
+export {createTab, executeFunction, sleep, closeTab, focusTab, doesTabExist, clickDeleteAllButton, clickConfirmDeleteButton, formScript};
