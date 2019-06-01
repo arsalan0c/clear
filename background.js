c8bf34c5f2bf733cecdfb5c5837c1aa74be4864b
Arsalan Cheema <arsalanc.v2@gmail.com>
2019-06-01 13:20:10 +0800

    format

--- background.js
+++ background.js
@@ -9,58 +9,64 @@ import {
   doesTabExist,
   clickDeleteAllButton,
   clickConfirmDeleteButton,
-  formScript,
-} from './helper.js';
+  formScript
+} from "./helper.js";
 
 const SEARCH_ACTIVITY_URL =
-  'https://myactivity.google.com/privacyadvisor/search';
+  "https://myactivity.google.com/privacyadvisor/search";
 const SLEEP_DURATION = 1000;
 
-const POS_RESULT_MSG = 'Result: your search activity has been deleted.';
-const NEG_RESULT_MSG = 'Result: your search activity remains with Google.';
-const CREATE_TAB_ERR = 'Error: failed to create a new tab.';
-const DELETE_ERR = 'Error: failed to delete.';
-const CLOSE_ERR = 'Error: failed to close created tab.';
-const RETURN_ERR = 'Error: failed to switch back to original tab.';
+const POS_RESULT_MSG = "Result: your search activity has been deleted.";
+const NEG_RESULT_MSG = "Result: your search activity remains with Google.";
+const CREATE_TAB_ERR = "Error: failed to create a new tab.";
+const DELETE_ERR = "Error: failed to delete.";
+const CLOSE_ERR = "Error: failed to close created tab.";
+const RETURN_ERR = "Error: failed to switch back to original tab.";
 
-chrome.browserAction.onClicked.addListener((originalTab) => {
+chrome.browserAction.onClicked.addListener(originalTab => {
   let newTabId;
 
-  createTab(SEARCH_ACTIVITY_URL, new Error(CREATE_TAB_ERR + '\n' +
-            NEG_RESULT_MSG))
-      .then((newTab) => {
-        if (!newTab) {
-          return Promise.reject(new Error(CREATE_TAB_ERR + '\n' +
-            NEG_RESULT_MSG));
-        }
-        newTabId = newTab.id;
-      })
-      .then(() =>
-        executeFunction(
-            newTabId,
-            formScript(clickDeleteAllButton),
-            new Error(DELETE_ERR + '\n' + NEG_RESULT_MSG)
-        )
+  createTab(
+    SEARCH_ACTIVITY_URL,
+    new Error(CREATE_TAB_ERR + "\n" + NEG_RESULT_MSG)
+  )
+    .then(newTab => {
+      if (!newTab) {
+        return Promise.reject(
+          new Error(CREATE_TAB_ERR + "\n" + NEG_RESULT_MSG)
+        );
+      }
+      newTabId = newTab.id;
+    })
+    .then(() =>
+      executeFunction(
+        newTabId,
+        formScript(clickDeleteAllButton),
+        new Error(DELETE_ERR + "\n" + NEG_RESULT_MSG)
       )
-      .then(() =>
-        executeFunction(
-            newTabId,
-            formScript(clickConfirmDeleteButton),
-            new Error(DELETE_ERR + '\n' + NEG_RESULT_MSG)
-        )
+    )
+    .then(() =>
+      executeFunction(
+        newTabId,
+        formScript(clickConfirmDeleteButton),
+        new Error(DELETE_ERR + "\n" + NEG_RESULT_MSG)
       )
-      .then(() => sleep(SLEEP_DURATION))
-      .then(() => closeTab(newTabId, new Error(CLOSE_ERR + '\n' +
-        POS_RESULT_MSG)))
-      .then(() => doesTabExist(originalTab.id))
-      .then((doesOriginalTabExist) => {
+    )
+    .then(() => sleep(SLEEP_DURATION))
+    .then(() =>
+      closeTab(newTabId, new Error(CLOSE_ERR + "\n" + POS_RESULT_MSG))
+    )
+    .then(() => doesTabExist(originalTab.id))
+    .then(doesOriginalTabExist => {
       // non-critical step
       // if tab does not exist, assumes user closed deliberately
-        if (doesOriginalTabExist) {
-          return focusTab(originalTab.id, new Error(RETURN_ERR + '\n' +
-            POS_RESULT_MSG));
-        }
-      })
-      .then(() => alert(POS_RESULT_MSG))
-      .catch((err) => alert(err.message));
+      if (doesOriginalTabExist) {
+        return focusTab(
+          originalTab.id,
+          new Error(RETURN_ERR + "\n" + POS_RESULT_MSG)
+        );
+      }
+    })
+    .then(() => alert(POS_RESULT_MSG))
+    .catch(err => alert(err.message));
 });
--- helper.js
+++ helper.js
@@ -1,46 +1,46 @@
 // helper.js
 
-let createTab = (url, err) => {
+const createTab = (url, err) => {
   return new Promise((resolve, reject) => {
     return chrome.tabs.create({ url: url, selected: true }, resolve);
   }).catch(() => Promise.reject(err));
 };
 
-let executeFunction = (tabId, script, err) => {
+const executeFunction = (tabId, script, err) => {
   return new Promise((resolve, reject) => {
     chrome.tabs.executeScript(tabId, { code: script }, resolve);
   }).catch(() => Promise.reject(err));
 };
 
-let sleep = ms =>
+const sleep = ms =>
   new Promise((resolve, reject) => {
     setTimeout(resolve, ms);
   });
 
-let closeTab = (tabId, err) => {
+const closeTab = (tabId, err) => {
   return new Promise((resolve, reject) => {
     chrome.tabs.remove(tabId, resolve);
   }).catch(() => Promise.reject(err));
 };
 
-let focusTab = (tabId, err) => {
+const focusTab = (tabId, err) => {
   return new Promise((resolve, reject) => {
     chrome.tabs.update(tabId, { selected: true }, resolve);
   }).catch(() => Promise.reject(err));
 };
 
-let doesTabExist = tabId => {
+const doesTabExist = tabId => {
   return new Promise((resolve, reject) => {
     chrome.tabs.get(tabId, resolve);
   }).catch(() => false);
 };
 
-let clickDeleteAllButton = () => {
+const clickDeleteAllButton = () => {
   // EXTENSION WON'T WORK IF THESE CHANGE
-  var DELETE_ALL_SELECTOR = "jsname";
-  var DELETE_ALL_SELECTOR_VALUE = "dQulXd";
+  const DELETE_ALL_SELECTOR = "jsname";
+  const DELETE_ALL_SELECTOR_VALUE = "dQulXd";
 
-  let button = document.querySelector(
+  const button = document.querySelector(
     "[" +
       DELETE_ALL_SELECTOR +
       "=" +
@@ -50,12 +50,12 @@ let clickDeleteAllButton = () => {
   if (button) button.click();
 };
 
-let clickConfirmDeleteButton = () => {
+const clickConfirmDeleteButton = () => {
   // EXTENSION WON'T WORK IF THESE CHANGE
-  var DELETE_CONFIRM_SELECTOR = "data-id";
-  var DELETE_CONFIRM_SELECTOR_VALUE = "EBS5u";
+  const DELETE_CONFIRM_SELECTOR = "data-id";
+  const DELETE_CONFIRM_SELECTOR_VALUE = "EBS5u";
 
-  let button = document.querySelector(
+  const button = document.querySelector(
     "[" +
       DELETE_CONFIRM_SELECTOR +
       "=" +
@@ -65,7 +65,7 @@ let clickConfirmDeleteButton = () => {
   if (button) button.click();
 };
 
-let formScript = (func, args) => {
+const formScript = (func, args) => {
   let script = "(" + func + ")";
   if (args && args.length > 0) {
     args.forEach(element => {
