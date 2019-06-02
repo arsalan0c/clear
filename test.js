2fa9a31d78a6929b2123472f74d732aabb440b43
Arsalan Cheema <arsalanc.v2@gmail.com>
2019-06-02 11:29:31 +0800

    change hook to precommit & prettier write & json for checks

--- test.js
+++ test.js
@@ -6,26 +6,13 @@ import { expect } from "chai";
 describe("form script test", function() {
   describe("without arguments", function() {
     it("should return 'good day'", function() {
-      let return_string = "good_day";
-      let func = function() {
-        return return_string;
+      const returnString = "good_day";
+      const func = function() {
+        return returnString;
       };
 
-      let script = formScript(func);
+      const script = formScript(func);
       expect(eval(script)).to.equal(func());
-    });
-  });
-
-  describe("with arguments", function() {
-    it("should return 'good day'", function() {
-      let arg1 = "good";
-      let arg2 = "day";
-      let func = function(arg1, arg2) {
-        return arg1 + " " + arg2;
-      };
-
-      let script = formScript(func, [arg1, arg2]);
-      expect(eval(script)).to.equal(func(arg1, arg2));
     });
   });
 });
